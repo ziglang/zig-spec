03f03a51ed297cccce4e78b59fe9192f82043980
Josh Wolfe <thejoshwolfe@gmail.com>
2021-02-26 09:46:51 -0500

    better error messages from check_parser.sh

--- grammar/check_parser.sh
+++ grammar/check_parser.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
 
+if [ ! -x ./build/parser ]; then
+	echo 'ERROR: ./build/parser: not found.'
+	echo 'ERROR: you need to run `make` first.'
+	exit 1
+fi
+
 ZIG=${ZIG:-zig}
 
 if [ $# -eq 0 ]; then
-	FILE_LIST=(./tests/*.zig)
-	FILES="${FILE_LIST[@]}"
+	FILES=(tests/*.zig)
 else
-	FILES="$@"
+	FILES=("$@")
 fi
 
-for FILE in $FILES
-do
-	$ZIG fmt --stdin < $FILE > /dev/null 2>&1
+echo "running ${#FILES[@]} tests..."
+
+ANY_FAILURE=
+for FILE in "${FILES[@]}"; do
+	$ZIG fmt --stdin < "$FILE" > /dev/null 2>&1
 	zigret=$?
-	./build/parser < $FILE > /dev/null 2>&1
+	./build/parser < "$FILE" > /dev/null 2>&1
 	parserret=$?
 
 	if [ "$zigret" -ne "$parserret" ]; then
-		echo "$FILE"
+		echo "FAIL: ${FILE}: zig: $zigret, grammar: $parserret"
+		ANY_FAILURE=1
 	fi
 done
+
+if [ -z "$ANY_FAILURE" ]; then
+	echo 'all tests passed'
+else
+	exit 1
+fi
